5e34d72fc4330f1dd4c0c2109f8dfa136af316d1
okabeharu <kd1338470@st.kobedenshi.ac.jp>
2024-05-23 09:19:44 +0900

    second commit

--- demo.py
+++ demo.py
@@ -6,17 +6,3 @@ st.title(':red[マーキュロ] :crossed_flags:')
 st.caption('2022年6月24日始動')
 st.caption('グループ名は消毒液であるマーキュロクロム液から由来病んだ傷を治す存在になれますように.')
 st.caption('A caption with _italics_ :blue[colors] and emojis :')
-
-col1, col2, col3 = st.beta_columns(3)
-
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", use_column_width=True)
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", use_column_width=True)
-    
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", use_column_width=True)
